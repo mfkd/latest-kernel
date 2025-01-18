ec06d8cb8fe9ff6393965904359963cbe4f7771a
mfkd <mattkfd@gmail.com>
2025-01-18 17:36:48 +0100

    Add example json fetch with client

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,67 @@
 const std = @import("std");
+const writer = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // Where we are going we need dynamic allocation
+    const alloc = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    const allocator = arena.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    //I like simple lifetimes so I am just clearing all allocations together
+    defer arena.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    //The client is what handles making and receiving the request for us
+    var client = std.http.Client{
+        .allocator = allocator,
+    };
 
-    try bw.flush(); // don't forget to flush!
+    //We can set up any headers we want
+    const headers = &[_]std.http.Header{
+        .{ .name = "X-Custom-Header", .value = "application" },
+        // if we wanted to do a post request with JSON payload we would add
+        // .{ .name = "Content-Type", .value = "application/json" },
+    };
+
+    // I moved this part into a seperate function just to keep it clean
+    const response = try get("https://www.kernel.org/releases.json", headers, &client, alloc);
+
+    // .ignore_unknown_fields will just omit any fields the server returns that are not in our type
+    // otherwise an unknown field causes an error
+    const result = try std.json.parseFromSlice(Result, allocator, response.items, .{ .ignore_unknown_fields = true });
+
+    try writer.print("title: {s}\n", .{result.value.title});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+//This is what we are going to parse the response into
+const Result = struct {
+    userId: i32,
+    id: i32,
+    title: []u8,
+    body: []u8,
+};
+
+fn get(
+    url: []const u8,
+    headers: []const std.http.Header,
+    client: *std.http.Client,
+    allocator: std.mem.Allocator,
+) !std.ArrayList(u8) {
+    try writer.print("\nURL: {s} GET\n", .{url});
+
+    var response_body = std.ArrayList(u8).init(allocator);
+
+    try writer.print("Sending request...\n", .{});
+    const response = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .url = url },
+        .extra_headers = headers, //put these here instead of .headers
+        .response_storage = .{ .dynamic = &response_body }, // this allows us to get a response of unknown size
+        // if we were doing a post request we would include the payload here
+        //.payload = "<some string>"
+    });
+
+    try writer.print("Response Status: {d}\n Response Body:{s}\n", .{ response.status, response_body.items });
+
+    // Return the response body to the caller
+    return response_body;
 }
